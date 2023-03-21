6ab526241c337bba72097fd790cea721124e8175
xiaozhuai <798047000@qq.com>
2023-03-21 16:52:49 +0800

    [libyuv] Use clang instead of msvc for x64 inline asm support

--- ports/libyuv/portfile.cmake
+++ ports/libyuv/portfile.cmake
@@ -7,6 +7,50 @@ vcpkg_from_git(
         cmake.diff
 )
 
+# use clang instead of msvc on windows
+vcpkg_cmake_get_vars(cmake_vars_file)
+include("${cmake_vars_file}")
+if (VCPKG_DETECTED_CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND NOT VCPKG_TARGET_IS_UWP)
+    # Most of libyuv accelerated features need to be compiled by clang/gcc, so force use clang-cl, otherwise the performance is too poor.
+    # Manually build the port with clang-cl when using MSVC as compiler
+
+    message(STATUS "Set compiler to clang-cl when using MSVC")
+
+    # https://github.com/microsoft/vcpkg/pull/10398
+    set(VCPKG_POLICY_SKIP_ARCHITECTURE_CHECK enabled)
+
+    vcpkg_find_acquire_program(CLANG)
+    if (CLANG MATCHES "-NOTFOUND")
+        message(FATAL_ERROR "Clang is required.")
+    endif ()
+    get_filename_component(CLANG "${CLANG}" DIRECTORY)
+
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+        set(CLANG_TARGET "arm")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        set(CLANG_TARGET "aarch64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        set(CLANG_TARGET "i686")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        set(CLANG_TARGET "x86_64")
+    else()
+        message(FATAL_ERROR "Unsupported target architecture")
+    endif()
+
+    set(CLANG_TARGET "${CLANG_TARGET}-pc-windows-msvc")
+
+    message(STATUS "Using clang target ${CLANG_TARGET}")
+    string(APPEND VCPKG_DETECTED_CMAKE_CXX_FLAGS --target=${CLANG_TARGET})
+    string(APPEND VCPKG_DETECTED_CMAKE_C_FLAGS --target=${CLANG_TARGET})
+
+    set(BUILD_OPTIONS
+            -DCMAKE_CXX_COMPILER=${CLANG}/clang-cl.exe
+            -DCMAKE_C_COMPILER=${CLANG}/clang-cl.exe
+            -DCMAKE_CXX_FLAGS=${VCPKG_DETECTED_CMAKE_CXX_FLAGS}
+            -DCMAKE_C_FLAGS=${VCPKG_DETECTED_CMAKE_C_FLAGS})
+endif ()
+##########################
+
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         tools BUILD_TOOLS
@@ -35,17 +79,5 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/libyuv-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-vcpkg_cmake_get_vars(cmake_vars_file)
-include("${cmake_vars_file}")
-if(VCPKG_DETECTED_CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    file(APPEND "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" [[
-
-Attention:
-You are using MSVC to compile libyuv. This build won't compile any
-of the acceleration codes, which results in a very slow library.
-See workarounds: https://github.com/microsoft/vcpkg/issues/28446
-]])
-endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
