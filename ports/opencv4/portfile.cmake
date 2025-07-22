9b5e2bc875d290df44e3b42f05bf161a33c846fb
xiaozhuai <798047000@qq.com>
2025-07-22 15:26:30 +0800

    [opencv4] Set debug log level to warning

--- ports/opencv4/portfile.cmake
+++ ports/opencv4/portfile.cmake
@@ -22,6 +22,7 @@ vcpkg_from_github(
       0015-fix-freetype.patch
       0017-fix-flatbuffers.patch
       0019-opencl-kernel.patch
+      9998-set-debug-log-level-to-warning.patch
 )
 
 vcpkg_find_acquire_program(PKGCONFIG)
