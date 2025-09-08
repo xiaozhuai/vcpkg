c4abc2abf2f654d5e82cdefd8a023a09f282afbc
xiaozhuai <798047000@qq.com>
2025-09-08 21:02:05 +0800

    [xiaozhuai-thorvg] New port

---
+++ ports/xiaozhuai-thorvg/portfile.cmake
@@ -0,0 +1,50 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO thorvg/thorvg
+    REF "v${VERSION}"
+    SHA512 2f25b36d5dffa2258a05616ea07fb881407a5d665d80696c8438854cb72ff52f6020c6e44a3eacde135dc0e5301a05aaca8a136204b03f5d3803fc638d2b38b4
+    HEAD_REF master
+    PATCHES
+        001-always-link-opengles.patch
+)
+
+if ("tools" IN_LIST FEATURES)
+    list(APPEND BUILD_OPTIONS -Dtools=all)
+endif()
+
+vcpkg_configure_meson(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${BUILD_OPTIONS}
+        # see ${SOURCE_PATH}/meson_options.txt
+        -Dstatic=true # Use static modules
+        -Dengines=['cpu','gl']
+        -Dloaders=all
+        -Dsavers=all
+        -Dsimd=true
+        -Dbindings=capi
+        -Dtests=false
+        -Dstrip=false
+        -Dextra=['opengl_es']
+    OPTIONS_DEBUG
+        -Dlog=false
+        -Dbindir=${CURRENT_PACKAGES_DIR}/debug/bin
+    OPTIONS_RELEASE
+        -Dbindir=${CURRENT_PACKAGES_DIR}/bin
+)
+vcpkg_install_meson()
+vcpkg_fixup_pkgconfig()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/thorvg-1/thorvg.h" "#ifndef TVG_STATIC" "#if 0")
+else()
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/thorvg-1/thorvg.h" "#ifndef TVG_STATIC" "#if 1")
+endif()
+
+if ("tools" IN_LIST FEATURES)
+    vcpkg_copy_tools(TOOL_NAMES tvg-svg2png tvg-lottie2gif AUTO_CLEAN)
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
